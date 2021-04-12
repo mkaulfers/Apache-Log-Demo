cc1ed7b562c0e88e2adc6eceedbe241175d25b9d
Matthew Kaulfers <mattk@ifit.com>
2021-04-12 11:19:07 -0400

    main: Initial project creation.

---
+++ Apache-Log-Demo/Apache-Log-Demo/Apache_Log_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apache_Log_DemoApp.swift
+//  Apache-Log-Demo
+//
+//  Created by Matthew Kaulfers on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Apache_Log_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
